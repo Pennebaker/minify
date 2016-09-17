8fa949ef355a6676517bd1fe25a82d1d642b3b3c
Andrew Welch <andrew@AmbrosiaSW.com>
2016-09-17 00:41:42 -0400

    [Improved] Updated for Craft3 changes

--- Minify.php
+++ Minify.php
@@ -10,22 +10,6 @@ class Minify extends \craft\app\base\Plugin
 {
 
     /**
-     * Static property that is an instance of this plugin class so that it can be accessed via Minify::$plugin
-     * @var craft\plugins\minify\Minify
-     */
-    public static $plugin;
-
-    /**
-     * Set our $plugin static property to this class so that it can be accessed via Minify::$plugin
-     * @param array $config [description]
-     */
-    public function __construct($id, $parent = null, $config = [])
-    {
-        Minify::$plugin = $this;
-        parent::__construct($id, $parent, $config);
-    }
-
-    /**
      * Initialize our plugin class; autoload any Composer-based vendor modules
      * @return [type] [description]
      */
